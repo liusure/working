9fe5794c18d2e718ff6a6d03594375484d503056
liushuo <liu.shuo@tenfen.com>
2018-12-01 23:00:12 +0800

    首页处理

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,14 @@
-alert("1npm")
+import 'swiper/dist/css/swiper.min.css'
+import '../css/index.less'
+import $ from "jquery"
+import swiper from "swiper"
+
+$(function () {
+    var mySwiper = new swiper('.swiper-container', {
+        loop: true,
+        autoplay: true,
+        pagination: {
+            el: '.swiper-pagination'
+        },
+    })
+})
--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,64 @@ var webpack = require('webpack');
 var path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: {
-    'js/index': './src/js/index.js'  //入口文件
-  },
-  output: {
-    filename: '[name].js',      //打包后index.js的名字，
-  },
-  //插件
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html"
-    }),
-    new CleanWebpackPlugin(['dist']),
-  ]
+    entry: {
+        index: path.join(__dirname, "./src/js/index.js")
+    },
+    output: {
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ttf|wtoff|eot|png|jpe?g|gif|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: "url-loader",
+                options: {
+                    limit: 10000,
+                    name: path.posix.join('static', 'img/[name].[hash:7].[ext]')
+                }
+            },
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader'
+                },
+                exclude: '/node_modules/'
+            }, {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            autoprefixer: true
+                        }
+                    }
+                ]
+            }, {
+                test: /\.less/,
+                use: [MiniCssExtractPlugin.loader,"css-loader", "less-loader"]
+            },
+        ]
+    },
+    //插件
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/page/index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "staticcss/app.[contenthash:12].css"
+        }),
+        // new CleanWebpackPlugin(['dist'])
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 9000,
+        open: true,
+        inline: true
+    }
 }
