333bdedcb29e71b7bc3ced30dbcf8428f3dbd2b0
liushuo <liu.shuo@tenfen.com>
2019-01-01 13:16:25 +0800

    第二版调整

--- src/js/news.js
+++ src/js/news.js
@@ -24,21 +24,26 @@ function pageRender(data) {
     let items = data.items;
     let content_container = $('.content_container');
     _.forEach(items, (item) => {
-        let date = new moment(item.updateTime).format('MM');
+        let date = new moment(item.updateTime).format('DD');
+        let month = new moment(item.updateTime).format('MMMM').substring(0,3);
+
         let ele = $(`
             <div class="news_item_container">
                 <div class="news_item_header">
                     <a href="newsDetail.html?id=${item.id}"><img class="news_img" src="${item.picUrl}" alt=""></a>
                 </div>
                 <div class="news_item_content">
-                    <div class="date">${date}</div>
+                    <div class="date">
+                    <span class="day">${date}</span>
+                    <span class="month">${month}</span>
+                    </div>
                     <div class="news_info">
                         <div class="news_title">${item.name}</div>
                         <div class="news_desc">${item.extensionProfile}</div>
                     </div>
                 </div>
                 <div class="news_item_bottom">
-                <a href="newsDetail.html?id=${item.id}" class="know_more">查看详情></a>
+                <a href="newsDetail.html?id=${item.id}" class="know_more">查看详情</a>
                 </div>
             </div>
         `)
